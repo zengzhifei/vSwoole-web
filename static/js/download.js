1f91c42852d87e877eb4450be127706508b23100
zengzhifei <zengzhifei@ctvit.com.cn>
2018-11-07 17:14:18 +0800

    add tongji

--- static/js/download.js
+++ static/js/download.js
@@ -6,6 +6,11 @@ new Vue({
                 vswoole_version: 'vSwoole-v1.2',
                 vswoole_date: '2018/8/30',
                 vswoole_url: 'https://github.com/zengzhifei/vSwoole/archive/v1.2.zip'
+            },
+            {
+                vswoole_version: 'vSwoole-v2.0.0',
+                vswoole_date: '2018/11/7',
+                vswoole_url: 'https://github.com/zengzhifei/vSwoole/archive/v2.0.0.zip'
             }
         ]
     }
---
+++ static/js/lib/tongji.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+    var hm = document.createElement("script");
+    hm.src = "https://hm.baidu.com/hm.js?749a7a5d3a4d6318a91f31804f9cfd9e";
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(hm, s);
+})();
