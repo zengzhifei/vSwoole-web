be30da512d35628e36a3f634c64e9bbaa2735e80
zengzhifei <zengzhifei@outlook.com>
2019-01-21 17:04:05 +0800

    add online count

--- static/js/lab.js
+++ static/js/lab.js
@@ -25,6 +25,7 @@ new Vue({
     mounted: function () {
         this.$nextTick(function () {
             this.initMPlayer();
+	    this.operateWebSocket();
         });
     },
     computed: {},
@@ -46,6 +47,14 @@ new Vue({
                             }
                         };
                         ws.send(JSON.stringify(range_data));
+
+			let get_online_data = {
+			    'cmd': 'online',
+			};
+			ws.send(JSON.stringify(get_online_data));
+			setInterval(()=>{
+			    ws.send(JSON.stringify(get_online_data));
+			},60000);
                     },
                     onMessage: (ws, res) => {
                         res = JSON.parse(res);
@@ -56,7 +65,14 @@ new Vue({
                             this.$nextTick(function () {
                                 this.$refs['WebSocket-chat-box'].scrollTop = this.$refs['WebSocket-chat-box'].scrollHeight;
                             });
-                        }
+                        } else if (res.type === 'online') {
+			    let data = res.data;
+		            let online_data = {chat_user: '在线小助手', chat_content: '当前有'+data+'人和你同时在线哦，快和他们聊聊吧～'};
+			    this.WebSocketChatList.push(online_data);
+                            this.$nextTick(function () {
+                                this.$refs['WebSocket-chat-box'].scrollTop = this.$refs['WebSocket-chat-box'].scrollHeight;
+                            });    
+			}
                     },
                     onClose: (ws) => {
                         this.WebSocketConnectStatus = 0;
@@ -104,22 +120,25 @@ new Vue({
             }, 1000);
         },
         getDanmakuList: function () {
-            let danmakuText = [
-                    '你好,陌生人',
-                    '你喜欢周杰伦吗',
-                    '想听一首周杰伦的歌吗',
-                    '这里为你准备了一首周杰伦的MV，点击播放就可以观看了',
-                    '怎么样，喜欢吗',
-                    '再见，陌生人'
-                ],
-                danmakuName = ['青花瓷', '半岛铁盒', '夜曲', '印第安老斑鸠', '一点点', '下沙'],
-                danmakuList = [];
-
-            for (let i = 0, max = Math.floor(Math.random() * 10); i <= max; i++) {
+	    let danmakuText = [
+                    '闻说双溪春尚好，也拟泛轻舟。只恐双溪舴艋舟，载不动许多愁。',
+                    '梧桐更兼细雨，到黄昏、点点滴滴。这次第，怎一个愁字了得！',
+                    '花自飘零水自流。一种相思，两处闲愁。此情无计可消除，才下眉头，却上心头。',
+                    '红藕香残玉簟秋。轻解罗裳，独上兰舟。云中谁寄锦书来，雁字回时，月满西楼。',
+                    '今年海角天涯，萧萧两鬓生华。看取晚来风势，故应难看梅花。',
+                    '天上星河转，人间帘幕垂。凉生枕簟泪痕滋。起解罗衣聊问、夜何其。',
+                    '更好明光宫殿，几枝先近日边匀。金尊倒，拚了尽烛，不管黄昏。',
+                    '记取楼前绿水，应念我、终日凝眸。凝眸处，从今更数，几段新愁。',
+                    '草际鸣蛩。惊落梧桐。正人间、天上愁浓。云阶月地，关锁千重。纵浮槎来，浮槎去，不相逢。',
+            ],
+            danmakuName = ['李清照'],
+            danmakuList = [];
+            
+	    for (let i = 0, max = Math.floor(Math.random() * 10); i <= max; i++) {
                 let j = Math.floor(Math.random() * danmakuText.length);
                 danmakuList.push({
                     img: './static/img/1.jpg',
-                    name: danmakuName[j],
+                    name: danmakuName[0],
                     text: danmakuText[j]
                 });
             }
